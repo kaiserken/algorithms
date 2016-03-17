29c62b833f377672f4ccb0f487f51756c648083d
Ken Kaiser <kkaiser@doublehelixllc.com>
2016-03-17 14:39:30 -0700

    recursion to cycle through nested object  - real world example

--- recursion.js
+++ recursion.js
@@ -1,8 +1,36 @@
-function numbers(num){
-	console.log(num);
-  if (num===10) return ;
-  
-  numbers(num+1);
+"use strict";
 
-}
-numbers(1);
+// function numbers(num){
+// 	console.log(num);
+//   if (num===10) return ;
+//   numbers(num+1);
+// }
+// numbers(1);
+//
+
+
+
+let categories  = [
+	{ id: 'animals', "parent": null},
+	{ id: 'mammals', "parent": 'animals'},
+	{ id: 'cats', "parent": "mammals"},
+	{ id: 'dogs', "parent": "mammals"},
+	{ id: 'collie', "parent": "dogs"},
+ 	{ id: 'yorkie', "parent": "dogs"},
+	{ id: 'persian', "parent": "cats"},
+	{ id: 'siamese', "parent": "cats"},
+];
+
+let makeTree = (categories, parent) => {
+	let node = {};
+	categories.filter(c => c.parent === parent)
+	.forEach(c => node[c.id]= makeTree(categories, c.id));
+	return node;
+
+};
+
+console.log(
+	JSON.stringify(
+			makeTree(categories, null), null, 2
+	)
+);
---
+++ txt.js
@@ -0,0 +1,4 @@
+Ken Kaiser	knife	200	1
+Ken Kaiser	waffle	300	2
+Mark Agosto	bench	300	4
+Mark Agosto rifle	700	1
